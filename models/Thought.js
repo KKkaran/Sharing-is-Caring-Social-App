63739045b561897165a57f06f176a956bdae8834
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-02-21 18:53:04 -0500

    thoughts and reaction schema created

---
+++ models/Thought.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose")
+const mongoose = require("mongoose")
+
+const reactionSchema = new Schema({
+
+    reactionId:{
+        type: mongoose.Types.ObjectId
+    },
+    reactionBody:{
+        type:String,
+        required:"A reactionBody is required",
+        maxlength:280,
+        trim:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+        //formatting the date to be done
+    }
+})
+
+const ThoughtSchema = new Schema({
+
+    thoughText:{
+        type:String,
+        required:"A thought is required",
+        maxlength:280,
+        trim:true
+    },
+    createdAt:{
+        type: Date,
+        default:Date.now
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    reactions:[reactionSchema]
+})
+
+// get total count of friends of the user
+ThoughtSchema.virtual('reactionsCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model("thought",ThoughtSchema)
+
+module.exports = Thought;
